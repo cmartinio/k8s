10f3f03a3ff0ab1afd945a27e86e4d551dbe2ba4
Christian Martin <fxgx99@gmail.com>
2022-12-07 01:03:08 +0100

    Working on the nginx endpoint module

---
+++ k8s-web-to-nginx/index.mjs
@@ -0,0 +1,19 @@
+import express from 'express'
+import fetch from 'node-fetch'
+import os from 'os'
+
+const app = express()
+const PORT = 3000
+
+app.get("/", (req, res) => {
+    const helloMessage = `Version: 02. Node Server For Kubernetes pinging from ${os.hostname()}`
+    console.log(helloMessage)
+    res.send(helloMessage)
+
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`web server is listening on port ${PORT}`)
+})
